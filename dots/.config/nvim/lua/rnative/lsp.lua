dbb441ef506d809ffcf9dded01b6af9211ab16f3
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-06-28 00:13:53 -0500

    add formatter from mason

--- dots/.config/nvim/lua/rnative/lsp.lua
+++ dots/.config/nvim/lua/rnative/lsp.lua
@@ -182,21 +182,38 @@ return {
   },
   -- NULL-LS
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "jay-babu/mason-null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.fish_indent,
-          nls.builtins.diagnostics.fish,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt,
-          -- nls.builtins.diagnostics.flake8,
+    dependencies = { "mason.nvim", "jose-elias-alvarez/null-ls.nvim" },
+    -- opts = function()
+    --   local nls = require("null-ls")
+    --   return {
+    --     root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+    --     sources = {
+    --       nls.builtins.formatting.fish_indent,
+    --       nls.builtins.diagnostics.fish,
+    --       nls.builtins.formatting.stylua,
+    --       nls.builtins.formatting.shfmt,
+    --       -- nls.builtins.diagnostics.flake8,
+    --       nls.builtins.formatting.autopep8,
+    --       nls.builtins.diagnostics.flake8,
+    --     },
+    --   }
+    -- end,
+    config = function()
+      require("mason").setup()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          -- Opt to list sources here, when available in mason.
         },
-      }
+        automatic_installation = false,
+        handlers = {},
+      })
+      require("null-ls").setup({
+        sources = {
+          -- Anything not supported by mason.
+        },
+      })
     end,
   },
   --
