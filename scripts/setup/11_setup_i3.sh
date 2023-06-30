05fa6cb19d2b2f9fdc2fb7f3a4a99dea3fb49948
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-06-30 15:33:18 -0500

    Add changes fo i3 and other

--- scripts/setup/11_setup_i3.sh
+++ scripts/setup/11_setup_i3.sh
@@ -28,4 +28,18 @@ sudo dnf install \
 	-y
 # Better locker
 wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | bash -s user
+# Remember activate the service
+# sudo systemctl enable betterlockscreen@
+# # # move service file to proper dir (the aur package does this for you)
+# cp betterlockscreen@.service /usr/lib/systemd/system/
+
+# # enable systemd service
+# systemctl enable betterlockscreen@$USER
+
+# # disable systemd service
+# systemctl disable betterlockscreen@$USER
+
+# # Note: Now you can call systemctl suspend to suspend your system
+# # and betterlockscreen service will be activated
+# # so when your system wakes your screen will be locked.
 
